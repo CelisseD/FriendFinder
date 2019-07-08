8c6fe921d49df10cbd3fd14205ba91856f95ffed
CelisseD <43502934+CelisseD@users.noreply.github.com>
2019-07-07 23:00:48 -0400

    added to friends array

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,9 +1,28 @@
 var friends = [
     {
-        "name" : "Zac",
-        "photo" : "https://i.pinimg.com/564x/0e/95/11/0e9511e35de28f0b5ebe5264afb6df9d.jpg",
+        "name" : "Justin",
+        "photo" : "https://s3.r29static.com//bin/entry/4c5/x,80/1705671/image.jpg",
         "scores" : [5,1,4,4,5,1,2,5,4,1]
     }
+
+    {
+        "name" : "Christina",
+        "photo" : "https://www.christinaaguilera.com/assets/img/xtina-pic-1.jpg",
+        "scores" : [3,1,4,2,4,5,2,4,2,1]
+    }
+
+    {
+        "name" : "Britney",
+        "photo" : "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BBVCZlW.img?h=328&w=624&m=6&q=60&o=f&l=f&x=639&y=312",
+        "scores" : [2,5,3,1,4,2,1,5,2,3]
+    }
+
+    {
+        "name" : "Ryan",
+        "photo" : "https://lovelace-media.imgix.net/getty/492408363.jpg",
+        "scores" : [1,2,3,4,5,1,2,3,4,5]
+    }
+    
 ];
 
 module.exports = friends;
